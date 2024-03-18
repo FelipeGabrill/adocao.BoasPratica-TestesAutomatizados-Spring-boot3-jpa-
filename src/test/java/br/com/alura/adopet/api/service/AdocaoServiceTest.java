a0aa3aae7581a62f9f8119da0f0f7bf870a41e94
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-03-18 20:24:31 -0300

    Validando objetos com ArgumentCaptor

---
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -0,0 +1,81 @@
+package br.com.alura.adopet.api.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.Tutor;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
+import br.com.alura.adopet.api.repository.PetRepository;
+import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+
+@ExtendWith(MockitoExtension.class)
+class AdocaoServiceTest {
+	
+	@InjectMocks
+	private AdocaoService service;
+
+	@Mock
+	private AdocaoRepository repository;
+
+	@Mock
+	private PetRepository petRepository;
+
+	@Mock
+	private TutorRepository tutorRepository;
+
+	@Mock
+	private EmailService emailService;
+
+	@Mock
+	private List<ValidacaoSolicitacaoAdocao> validacoes;
+	
+	@Mock 
+	private Pet pet;
+	
+	@Mock
+	private Tutor tutor;
+	
+	@Mock
+	private Abrigo abrigo;
+	
+	private SolicitacaoAdocaoDto dto;
+	
+	@Captor
+	private ArgumentCaptor<Adocao> adocaoCaptor;
+
+	@Test
+	void deveriaSalvarAdocaoAoSolicitar() {
+		// ARRANGE
+		this.dto = new SolicitacaoAdocaoDto(10l, 20l, "Motivo qualquer");
+
+		BDDMockito.given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+		BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+		BDDMockito.given(pet.getAbrigo()).willReturn(abrigo);
+		
+		// ACT
+		service.solicitar(dto);
+		
+		// ASSERT
+		BDDMockito.then(repository).should().save(adocaoCaptor.capture());
+		Adocao adocaoSalva = adocaoCaptor.getValue();
+		assertEquals(pet, adocaoSalva.getPet());
+		assertEquals(tutor, adocaoSalva.getTutor());
+		assertEquals(dto.motivo(), adocaoSalva.getMotivo());
+	}
+
+}
