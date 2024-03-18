ede9d32e21d9b78fa6419c51861b3f545fcb0344
Felipe Gabriel <felipegabrielodss@gmail.com>
2024-03-17 21:08:26 -0300

    Escrevendo um teste automatizado

---
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,30 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+	@Test
+	void cenario01() {
+	//idade 4 anos e 4kg
+	
+	Abrigo abrigo = new Abrigo(new CadastroAbrigoDto("Abrigo feliz", "94999999999", "abrigofeliz@gmail.com")); 
+	
+	Pet pet = new Pet(new CadastroPetDto(TipoPet.GATO, "miau", "Siames", 4, "Cinza", 4.0f), abrigo);
+		
+	CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+	ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+	
+	Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidade);
+	
+	}
+	
+}
